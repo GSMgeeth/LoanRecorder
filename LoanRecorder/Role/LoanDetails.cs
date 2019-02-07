3353adf274afdfb402e08e318f71e31a96232e64
Geeth Sandaru Madhushan <geeth.gsm@gmail.com>
2019-02-07 22:19:47 +0530

    person and guarantor classes

--- LoanRecorder/Role/LoanDetails.cs
+++ LoanRecorder/Role/LoanDetails.cs
@@ -8,5 +8,6 @@ namespace LoanRecorder.Role
 {
     class LoanDetails
     {
+
     }
 }
--- LoanRecorder/Role/Person.cs
+++ LoanRecorder/Role/Person.cs
@@ -8,5 +8,50 @@ namespace LoanRecorder.Role
 {
     class Person
     {
+        private long pid;
+        private string name;
+        private string nic;
+        private string tel;
+        private string address;
+        private Guarantor [] guarantors = new Guarantor[2];
+
+        public Person()
+        {
+            
+        }
+
+        public Person(Guarantor[] guarantors)
+        {
+            Guarantors = guarantors;
+        }
+
+        public Person(long pid, string name, string nic, string tel, string address)
+        {
+            this.Pid = pid;
+            this.Name = name;
+            this.Nic = nic;
+            this.Tel = tel;
+            this.Address = address;
+        }
+
+        public Person(string name, string nic, string tel, string address)
+        {
+            this.Name = name;
+            this.Nic = nic;
+            this.Tel = tel;
+            this.Address = address;
+        }
+
+        public Person(long pid)
+        {
+            this.Pid = pid;
+        }
+
+        public long Pid { get => pid; set => pid = value; }
+        public string Name { get => name; set => name = value; }
+        public string Nic { get => nic; set => nic = value; }
+        public string Tel { get => tel; set => tel = value; }
+        public string Address { get => address; set => address = value; }
+        internal Guarantor[] Guarantors { get => guarantors; set => guarantors = value; }
     }
 }
