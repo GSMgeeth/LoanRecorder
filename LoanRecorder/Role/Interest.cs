560fbe831c2329bde69de9624f16c20e166d252b
Geeth Sandaru Madhushan <geeth.gsm@gmail.com>
2019-02-07 22:47:12 +0530

    added interest class and common folder with global class

---
+++ LoanRecorder/Role/Interest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanRecorder.Role
+{
+    class Interest
+    {
+        private int interestId;
+        private double interestPercentage;
+
+        public Interest()
+        {
+        }
+
+        public Interest(double interestPercentage)
+        {
+            this.InterestPercentage = interestPercentage;
+        }
+
+        public Interest(int interestId, double interestPercentage)
+        {
+            this.InterestId = interestId;
+            this.InterestPercentage = interestPercentage;
+        }
+
+        public int InterestId { get => interestId; set => interestId = value; }
+        public double InterestPercentage { get => interestPercentage; set => interestPercentage = value; }
+    }
+}
